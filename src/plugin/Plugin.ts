8aeb259bdfc2594ee1846901854dc2905668b598
naokiy <to_contact@naokiy.net>
2023-07-27 19:15:27 +0000

    chore: :art: Apply formatter

--- src/plugin/Plugin.ts
+++ src/plugin/Plugin.ts
@@ -3,10 +3,9 @@ import { PluginSettingTab } from "./PluginSettingTab";
 import * as obsidian from "obsidian";
 
 export class Plugin extends obsidian.Plugin {
-    onload() {
-        this.addSettingTab(new PluginSettingTab(this));
-	}
+  onload() {
+    this.addSettingTab(new PluginSettingTab(this));
+  }
 
-	onunload() {
-	}
+  onunload() {}
 }
--- src/plugin/PluginSettingTab.ts
+++ src/plugin/PluginSettingTab.ts
@@ -3,14 +3,12 @@ import type { Plugin } from "./Plugin";
 import * as obsidian from "obsidian";
 
 export class PluginSettingTab extends obsidian.PluginSettingTab {
-    plugin: Plugin;
-    
-    constructor(plugin: Plugin) {
-        super(plugin.app, plugin);
-        this.plugin = plugin;
-    }
+  plugin: Plugin;
 
-    display() {
-        
-    }
+  constructor(plugin: Plugin) {
+    super(plugin.app, plugin);
+    this.plugin = plugin;
+  }
+
+  display() {}
 }
